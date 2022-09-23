c541082b9fc962e2ab5074eaf87350161d07ef44
Franck59840 <depoorter.franck@gmail.com>
2022-09-23 16:50:37 +0200

    Create label object and add translate

--- src/components/LabelTech.vue
+++ src/components/LabelTech.vue
@@ -1,14 +1,16 @@
 /
 <template>
   <v-container>
-    <v-btn
-      v-for="label in labels"
-      :key="label.id"
-      elevation="2"
-      class="mt-2 ml-2 test rounded-lg"
-    >
-      {{ label }}
-    </v-btn>
+    <div class="d-flex flex-wrap">
+      <v-btn
+        v-for="label in labels"
+        :key="label.id"
+        elevation="2"
+        class="mt-2 ml-2 test rounded-lg"
+      >
+        {{ $t(label.translateId) }}
+      </v-btn>
+    </div>
   </v-container>
 </template>
 
@@ -17,41 +19,99 @@ export default {
   data: () => ({
     model: 0,
     labels: [
-      "HTML5",
-      "CSS3",
-      "Javascript",
-      "React",
-      "Node JS",
-      "PHP",
-      "Symfony",
-      "MySQL",
-      "GIT",
-      "Github",
-      "SCRUM",
-      "Formation",
-      "Product Owner",
-      "Gestion de projet",
-      "Esprit d'équipe",
-      "Communication",
-      "Organisation",
-      "Apprendre",
-      "Autonomie",
-      "Linux",
-      "No Code",
-      "Bubble",
-      "Figma",
-      "Notion",
-      "Zappier",
-      "Webflow",
-      "Airtable",
+      {
+        translateId: "label.html",
+      },
+      {
+        translateId: "label.css",
+      },
+      {
+        translateId: "label.js",
+      },
+      {
+        translateId: "label.react",
+      },
+      {
+        translateId: "label.node",
+      },
+      {
+        translateId: "label.php",
+      },
+      {
+        translateId: "label.symfony",
+      },
+      {
+        translateId: "label.crud",
+      },
+      {
+        translateId: "label.mysql",
+      },
+      {
+        translateId: "label.git",
+      },
+      {
+        translateId: "label.github",
+      },
+      {
+        translateId: "label.scrum",
+      },
+      {
+        translateId: "label.formation",
+      },
+      {
+        translateId: "label.po",
+      },
+      {
+        translateId: "label.project",
+      },
+      {
+        translateId: "label.teamspirit",
+      },
+      {
+        translateId: "label.teamwork",
+      },
+      {
+        translateId: "label.communication",
+      },
+      {
+        translateId: "label.organisation",
+      },
+      {
+        translateId: "label.learn",
+      },
+      {
+        translateId: "label.independance",
+      },
+      {
+        translateId: "label.vscode",
+      },
+      {
+        translateId: "label.linux",
+      },
+      {
+        translateId: "label.nocode",
+      },
+      {
+        translateId: "label.bubble",
+      },
+      {
+        translateId: "label.figma",
+      },
+      {
+        translateId: "label.notion",
+      },
+      {
+        translateId: "label.zappier",
+      },
+      {
+        translateId: "label.webflow",
+      },
+      {
+        translateId: "label.airtable",
+      },
     ],
   }),
 };
 </script>
 
-<style>
-.test {
-  border: solid 2px #677494;
-  color: white;
-}
-</style>
+<style></style>
